a24cde95a975547cd58c6c1d5c66464bec0bb240
David Steele <dsteele@gmail.com>
2018-08-27 22:11:13 -0400

    Fix setup.py pytest

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     entry_points={"console_scripts": ["txttomp3 = txttomp3.txttomp3:main"]},
     data_files=[("/var/cache/txttomp3", [])],
     python_requires=">=2.7,<3.0",
+    setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     classifiers=[
     ],
